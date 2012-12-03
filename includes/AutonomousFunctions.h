a44c6053c6c21650c70c69f94738608a7ae84de1
frankslaughter <fgs@tower3.com>
2012-12-02 17:28:24 -0700

    Added a new button to teleop. Started autonomous, but still needs help.

--- includes/AutonomousFunctions.h
+++ includes/AutonomousFunctions.h
@@ -8,44 +8,49 @@
 
 void moveStraight(int power, int duration)
 {
-  motor[rightSide] = power;
-  motor[leftSide] = power;
+  motor[rightRear] = power;
+  motor[rightFrontPair] = power;
+  motor[leftRear] = power;
+  motor[leftFrontPair] = power;
   wait1Msec(duration);
-  motor[rightSide] = 0;
-  motor[leftSide] = 0;
+  motor[rightRear] = 0;
+  motor[rightFrontPair] = 0;
+  motor[leftRear] = 0;
+  motor[leftFrontPair] = 0;
 }
 
 void move(int powerRight, int powerLeft, int duration)
 {
-  motor[rightSide] = powerRight;
-  motor[leftSide] = powerLeft;
+  motor[rightRear] = powerRight;
+  motor[rightFrontPair] = powerRight;
+  motor[leftRear] = powerLeft;
+  motor[leftFrontPair] = powerLeft;
   wait1Msec(duration);
-  motor[rightSide] = 0;
-  motor[leftSide] = 0;
+  motor[rightRear] = 0;
+  motor[rightFrontPair] = 0;
+  motor[leftRear] = 0;
+  motor[leftFrontPair] = 0;
+}
+
+void moveArm (int power, int duration)
+{
+	motor[shoulderJoint] = power;
+	wait1Msec(duration);
+	motor[shoulderJoint] = 0;
+}
+
+void moveHand(int position)
+{
+	servo[handJoint] = position;
 }
 
 void allStop()
 {
-  motor[rightSide] = 0;
-  motor[leftSide] = 0;
+  motor[rightRear] = 0;
+  motor[rightFrontPair] = 0;
+  motor[leftRear] = 0;
+  motor[leftFrontPair] = 0;
   motor[shoulderJoint] = 0;
-  motor[elbowJoint] = 0;
-  motor[ballHopper] = 0;
-}
-
-void deployHopper(bool isUp, int power, int duration)
-{
-  if (isUp)
-  {
-    motor[ballHopper] = power;
-    wait1Msec(duration);
-  }
-  if (isUp == false)
-  {
-    motor[ballHopper] = -1*power;
-    wait1Msec(duration);
-  }
-  motor[ballHopper] = 0;
 }
 
 bool buttonValid(int btn)
@@ -78,91 +83,3 @@ bool buttonValid(int btn)
   }
   return false;
 }
-
-void deployArm()
-{
-  deployHopper(false,10,500);
-  motor[elbowJoint] = 20;
-  wait1Msec(500);
-  motor[elbowJoint] = 0;
-  deployHopper(false,20,500);
-  motor[ballHopper] = 0;
-  motor[shoulderJoint] = -20;
-  wait1Msec(400);
-  motor[shoulderJoint] = 0;
-}
-
-
-void outer(bool isBlue)
-{
-  if (isBlue)
-  {
-    moveStraight(10,5000);
-    deployArm();
-    wait10Msec(75);
-    move(-50,50, 1000);
-    moveStraight(10,500);
-    move(50,-50, 1000);
-    deployHopper(true,30,500);
-    wait10Msec(75);
-    moveStraight(20,5000);
-  }
-  else
-  {
-    moveStraight(10,5000);
-    deployArm();
-    wait10Msec(75);
-    move(50,-50, 1000);
-    moveStraight(10,500);
-    move(-50,50, 1000);
-    deployHopper(true,30,500);
-    wait10Msec(75);
-    moveStraight(20,5000);
-  }
-}
-
-
-void hit_crates(bool isBlue)
-{
-  if (isBlue)
-  {
-    moveStraight(10,5000);
-    deployArm();
-    wait10Msec(75);
-    move(-50,50,750);
-    moveStraight(50,1000);
-  }
-  else
-  {
-    moveStraight(10,5000);
-    deployArm();
-    wait10Msec(75);
-    move(50,-50,750);
-    moveStraight(50,1000);
-  }
-}
-
-void hit_bowling_ball(bool isBlue)
-{
-  if (isBlue)
-  {
-    move(-50,50,750);
-    moveStraight(50,5000);
-  }
-  else
-  {
-    move(50,-50,750);
-    moveStraight(50,5000);
-  }
-
-}
-
-void bowling_ball(bool isBlue)
-{
-  hit_crates(isBlue);
-  hit_bowling_ball(isBlue);
-}
-
-
-
-
